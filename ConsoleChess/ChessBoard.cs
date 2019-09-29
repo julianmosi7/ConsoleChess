795d679dcb91f272b53b8939f764424e2f98b3eb
Julian Moshammer <julianmosi@Julians-MacBook-Pro.local>
2019-09-29 12:25:11 +0200

    show board

--- ConsoleChess/ChessBoard.cs
+++ ConsoleChess/ChessBoard.cs
@@ -6,10 +6,24 @@ namespace ConsoleChess
 {
     class ChessBoard
     {
+        static int fields;
         public void Show()
         {
-            ColorWriter greytogrey = new ColorWriter() { ForeGroundColor = ConsoleColor.Black, BackGroundColor = ConsoleColor.Gray };
-            greytogrey.Write("--------------------------------");
+            ColorWriter blacktogrey = new ColorWriter() { ForeGroundColor = ConsoleColor.Black, BackGroundColor = ConsoleColor.Gray };
+            Console.Write(" ");
+            blacktogrey.WriteLine("-----------------------------------------");
+            for (fields = 8; fields > 1; fields--)
+            {
+                Console.Write($"{fields}");
+                blacktogrey.WriteLine("|    |    |    |    |    |    |    |    |");
+                Console.Write(" ");
+                blacktogrey.WriteLine("|----+----+----+----+----+----+----+----|");
+            }
+            Console.Write($"{fields}");
+            blacktogrey.WriteLine("|    |    |    |    |    |    |    |    |");
+            Console.Write(" ");
+            blacktogrey.WriteLine("-----------------------------------------");
+            Console.WriteLine("   a    b    c    d    e    f    g    h");
         }
     }
 }
