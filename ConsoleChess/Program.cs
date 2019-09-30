30d0f60c23b84bf0df5d3a66e4cd71d7318e819c
julianmosi7 <moshammer.ju@gmail.com>
2019-09-30 18:06:36 +0200

    readmove and changecolor in readmove

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -8,15 +8,17 @@ namespace ConsoleChess
         {
             string move;
             bool player_won = false;
-            string color = "black";
+            string color = "white";
             ChessBoard chessBoard = new ChessBoard();
             chessBoard.Show();
-            for (int round = 0; player_won == false; round++)
+            for (int round = 1; player_won == false; round++)
             {
-                //1. Zug weiß, 2. Zug black, 3. Zug weiß -> false  f|r  1. Zug weiß, 1. Zug black, 2. Zug weiß, ...
-                //if verzweigung als einzeiler mit ? und :
+                //1. Zug weiß, 2. Zug black, 3. Zug weiß -> false  f|r  1. Zug weiß, 1. Zug black, 2. Zug weiß, ...                
                 color = color.Equals("black") ? "black" : "white";
                 move = ReadMove(round, color);
+                color = ChangeColor(color);
+                move = ReadMove(round, color);                
+                color = ChangeColor(color);
             }
         }
 
@@ -24,8 +26,14 @@ namespace ConsoleChess
         {
             string move;
             Console.WriteLine($"{round}. Zug {color}:");
-            move = Console.ReadLine();
+            move = Console.ReadLine();         
             return move;
+        }    
+        
+        private static string ChangeColor(string color)
+        {
+            color = color.Equals("black") ? "white" : "black";
+            return color;
         }
     }
 }
